6a74f198cd232b1f49a23dd63f9cafc29271e05b
DTang127 <93285387+DTang127@users.noreply.github.com>
2023-10-05 17:53:08 -0400

    updates dashboard

--- app.py
+++ app.py
@@ -12,6 +12,8 @@ app.layout = html.Div([
     html.Div(children= "Sleep Analysis"),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
     dcc.Graph(figure=px.histogram(df, x=df.Occupation.sort_values(), title='Sleep Occupation'))
+    dcc.Graph(figure=px.histogram(df, x=test.index, y=test.values, 
+             labels={"x": "Occupation", "y": "Avg Sleep Duration"}, title='Avg Sleep Duration'))
 ])
 
 if __name__ == '__main__':
