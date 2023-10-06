c5f0bcb39c064ea967bdd271cdb3ea32d86413c3
falselysium <lancerxancer@gmail.com>
2023-10-05 21:27:26 -0400

    code: Fixed up occupation, avg sleep duration graph

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 # Sleep data
 df = pd.read_csv('./sleep_dataset.csv')
+df.sort_values('Occupation', inplace=True)
 
 # Initialize the app -> team name is ada
 app = Dash(__name__)
@@ -11,9 +12,8 @@ app = Dash(__name__)
 app.layout = html.Div([
     html.Div(children= "Sleep Analysis"),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
-    dcc.Graph(figure=px.histogram(df, x=df.Occupation.sort_values(), title='Sleep Occupation'))
-    dcc.Graph(figure=px.histogram(df, x=test.index, y=test.values, 
-             labels={"x": "Occupation", "y": "Avg Sleep Duration"}, title='Avg Sleep Duration'))
+    dcc.Graph(figure=px.histogram(df, x=df['Occupation'], title='Sleep Occupation')),
+    dcc.Graph(figure=px.histogram(df, x=df['Occupation'].unique(), y=df.groupby('Occupation')['Sleep Duration'].mean().round(2), labels={"x": "Occupation", "y": "Avg Sleep Duration"}, title='Avg Sleep Duration'))
 ])
 
 if __name__ == '__main__':
